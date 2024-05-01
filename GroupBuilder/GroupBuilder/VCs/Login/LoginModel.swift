db3542656dea41cd80d7dfa62f1357daa16f660c
kcwjy90 <kcwjy90@gmail.com>
2024-04-30 23:01:54 -0400

    LoginVC created. Import Firebase, snapkit

---
+++ GroupBuilder/GroupBuilder/VCs/Login/LoginModel.swift
@@ -0,0 +1,12 @@
+//
+//  LoginModel.swift
+//  GroupBuilder
+//
+//  Created by ppc90 on 4/30/24.
+//
+
+import UIKit
+
+class LoginModel: NSObject {
+
+}
---
+++ GroupBuilder/GroupBuilder/VCs/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  GroupBuilder
+//
+//  Created by ppc90 on 4/30/24.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
