6d4f79320a4effbc356708c5b6f224d14b91fa14
kcwjy90 <kcwjy90@gmail.com>
2024-05-09 19:35:32 -0400

    LoginView - added title, image, AppleLogin Button.

--- GroupBuilder/GroupBuilder/VCs/Login/LoginView.swift
+++ GroupBuilder/GroupBuilder/VCs/Login/LoginView.swift
@@ -9,13 +9,32 @@ import UIKit
 import SnapKit
 
 class LoginView: UIView {
-
+    
     lazy var title: UILabel = {
         let v = UILabel()
-        v.text = "GROUP"
+        v.text = "GroupBuilder"
         v.textColor = .white
-        v.font = UIFont.systemFont(ofSize: 30)
+        v.font = UIFont.systemFont(ofSize: 50)
         v.textAlignment = .center
+        return v
+    }()
+    
+    lazy var addIconLater: UILabel = {
+        let v = UILabel()
+        v.text = "G"
+        v.textColor = .white
+        v.font = UIFont.systemFont(ofSize: 50)
+        v.textAlignment = .center
+        return v
+    }()
+    
+    lazy var appleLoginButton: UIButton = {
+        let v = UIButton()
+        v.setImage(UIImage(named: "apple"), for: .normal)
+        v.setTitle(" Continue with Apple", for: .normal)
+        v.frame = CGRect(x: 0, y: 200, width: 200, height: 50)
+        v.backgroundColor = .black
+        v.layer.cornerRadius = 10
         return v
     }()
     
@@ -23,12 +42,27 @@ class LoginView: UIView {
         super.init(frame: .zero)
         
         addSubview(title)
+        addSubview(addIconLater)
+        addSubview(appleLoginButton)
         
         title.snp.makeConstraints{ (make) in
             make.centerX.equalTo(self)
-            make.top.equalTo(self).offset(-100)
-            make.width.equalTo(200)
-            make.height.equalTo(50)
+            make.top.equalTo(self).offset(250)
+        }
+        
+        addIconLater.snp.makeConstraints{ (make) in
+            make.centerX.equalTo(title)
+            make.centerY.equalTo(self)
+            make.width.equalTo(100)
+            make.height.equalTo(100)
+        }
+        addIconLater.backgroundColor = .systemPurple
+        
+        appleLoginButton.snp.makeConstraints{ (make) in
+            make.centerX.equalTo(title)
+            make.bottom.equalTo(addIconLater).offset(200)
+            make.width.equalTo(303)
+            make.height.equalTo(54)
         }
         backgroundColor = .systemOrange
 
