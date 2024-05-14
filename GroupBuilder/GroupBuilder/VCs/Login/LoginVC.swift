08515bdefd1a986381f0bb1450e065075e03c013
kcwjy90 <kcwjy90@gmail.com>
2024-05-14 00:14:46 -0400

    LoginVC - added TabBarController components. the code works fine (checked in AppDelegate) but currently not displaying possibly b/c I still need to add button ->  @objc func that activates the button -> present tabbar when login is succeeded.

--- GroupBuilder/GroupBuilder/VCs/Login/LoginVC.swift
+++ GroupBuilder/GroupBuilder/VCs/Login/LoginVC.swift
@@ -10,17 +10,47 @@ import FirebaseCore
 import FirebaseAuth
 import AuthenticationServices
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, UITabBarControllerDelegate {
 
     lazy var contentView: LoginView = LoginView()
     let model: LoginModel = LoginModel()
+
+    //For Tabbar (1/2)
+    let tabBarCtlr = UITabBarController()
+    
+    let todayVC: TodayVC = TodayVC()
+    let calendarVC: CalendarVC = CalendarVC()
+    let progressVC: ProgressVC = ProgressVC()
     
     override func loadView() {
         super.loadView()
         
         view.addSubview(contentView)
         contentView.frame = view.frame
- 
+        
+        navigationVCs()
+        
+    }
+    
+    func navigationVCs() {
+        
+        //For Tabbar (2/2)
+        tabBarCtlr.tabBar.isTranslucent = false //both lines needed
+        tabBarCtlr.tabBar.backgroundColor = .white //to change tabbar color
+        UITabBar.appearance().tintColor = .black
+        
+        let todayNavi = UINavigationController(rootViewController: todayVC)
+        todayVC.title = "Today"
+        
+        let calendarNavi = UINavigationController(rootViewController: calendarVC)
+        calendarVC.title = "Calendar"
+        
+        let progressNavi = UINavigationController(rootViewController: progressVC)
+        progressVC.title = "Progress"
+        
+        tabBarCtlr.modalPresentationStyle = .fullScreen
+        tabBarCtlr.setViewControllers([todayNavi, calendarNavi, progressNavi], animated: false)
+        
     }
 
 }
