9557149dc74528ffd689a3039be6bc51228b1e0f
kcwjy90 <kcwjy90@gmail.com>
2024-05-15 18:53:51 -0400

    LoginVC - added @objc func to display TabBarController and all the designated VCs when apple login button is clicked. Apple Login hasn't been incorporated yet. Image Assets for TabBar have been added.

--- GroupBuilder/GroupBuilder/VCs/Login/LoginVC.swift
+++ GroupBuilder/GroupBuilder/VCs/Login/LoginVC.swift
@@ -18,39 +18,52 @@ class LoginVC: UIViewController, UITabBarControllerDelegate {
     //For Tabbar (1/2)
     let tabBarCtlr = UITabBarController()
     
-    let todayVC: TodayVC = TodayVC()
-    let calendarVC: CalendarVC = CalendarVC()
-    let progressVC: ProgressVC = ProgressVC()
     
     override func loadView() {
         super.loadView()
         
         view.addSubview(contentView)
+        
         contentView.frame = view.frame
+        contentView.appleLoginButton.addTarget(self, action: #selector(appleLoginButtonPressed), for: .touchUpInside)
         
         navigationVCs()
-        
+            
     }
     
     func navigationVCs() {
         
-        //For Tabbar (2/2)
+        let todayVC: TodayVC = TodayVC()
+        let calendarVC: CalendarVC = CalendarVC()
+        let progressVC: ProgressVC = ProgressVC()
+        
+        //==For Tabbar (2/2)========================
         tabBarCtlr.tabBar.isTranslucent = false //both lines needed
         tabBarCtlr.tabBar.backgroundColor = .white //to change tabbar color
         UITabBar.appearance().tintColor = .black
+        //==========================================
         
         let todayNavi = UINavigationController(rootViewController: todayVC)
         todayVC.title = "Today"
+        todayVC.tabBarItem.image = UIImage(named: "today")
         
         let calendarNavi = UINavigationController(rootViewController: calendarVC)
         calendarVC.title = "Calendar"
+        calendarVC.tabBarItem.image = UIImage(named: "calendar")
         
         let progressNavi = UINavigationController(rootViewController: progressVC)
         progressVC.title = "Progress"
+        progressVC.tabBarItem.image = UIImage(named: "progress")
         
         tabBarCtlr.modalPresentationStyle = .fullScreen
         tabBarCtlr.setViewControllers([todayNavi, calendarNavi, progressNavi], animated: false)
         
+    }
+    
+    @objc func appleLoginButtonPressed() {
+        
+        self.present(self.tabBarCtlr, animated: true) // LoginVC -> TabBar
+ 
     }
 
 }
