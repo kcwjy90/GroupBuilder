ee090c10a5f79ca2b38d2dc6e3208966016d3fe6
kcwjy90 <kcwjy90@gmail.com>
2024-05-16 18:46:43 -0400

    LoginVC - added authorizationController in appleLoginButtonPressed(). Added ASAuthorizationcontrollerDelegate extension to pass idToken and appleCredential to Model LoginModel - created enum NeedToGetAuthStatus to direct where to go depending on the status of applelogin. Created fun starSignInWithAppleFlow with nonce, sha256 to get apple authentication. It returns authorizationController to LoginVC. Created authApple function. If successfully logins, it directs to presentTabBar

--- GroupBuilder/GroupBuilder/VCs/Login/LoginModel.swift
+++ GroupBuilder/GroupBuilder/VCs/Login/LoginModel.swift
@@ -12,6 +12,169 @@ import CryptoKit
 import AuthenticationServices
 import FirebaseDatabase
 
-class LoginModel: NSObject {
+// to be used in AuthApple ()
+enum NeedToGetAuthStatus {
+    case needToGetUserName
+    case presentTabBar
+    case none
+}
 
+class LoginModel: NSObject {
+    
+    var currentNonce: String?
+    
+    override init() {
+        super.init()
+        
+    }
+    
+    /* === Step 2. nonce 만들고, sha256 처리
+     nonce: random number to prevent replay attacks
+     Sha256: hash function that takes a string and spits out a value that is 256 bits long.
+     보안을 위한 암호화. another layer of protection on top of Apple Login
+     === */
+    func startSignInWithAppleFlow() -> ASAuthorizationController {
+        
+        let nonce = randomNonceString()
+        currentNonce = nonce //멤버변수로
+        
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationContoller = ASAuthorizationController(authorizationRequests: [request])
+        
+        return authorizationContoller
+        
+    }
+    
+    // =====Copy & Pasted to use generate Nonce and Sha256. Adapted from=== https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: [Character] = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError(
+                        "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+                    )
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        return result
+    }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+    
+    //===================================================================
+    
+    //Apple Authorization
+    func authApple(idTokenString: String,
+                   appleIDCredential: ASAuthorizationAppleIDCredential,
+                   completion: @escaping ((NeedToGetAuthStatus, Error?) -> Void)) {
+        
+        //appleIDCredential and idTokenString from LoginVC
+        let fbCredential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                    idToken: idTokenString,
+                                                    rawNonce: currentNonce)
+        
+        let appleCredential = appleIDCredential.user
+        
+//                DELETE after adding FB ===
+        let error = NSError(domain: "ERROR", code: -1)
+        completion(.presentTabBar, error)
+//                DELETE after adding FB ===
+
+//                5/16 일단 commented===
+
+        /* === Step 4. Firebase Login 시도 - Initialize a Firebase credential ========
+         2번의 nonce, 3번의 appleId, idToken 전달
+         */
+        
+//        Auth.auth().signIn(with: fbCredential) { (authResult, error) in
+//            if let error = error {
+//                completion(.none, error)
+//                return
+//            }
+            
+            
+            /* === Step 5. Firebase Login 성공: Access_token 전달받음 ======== */
+//            print("Firebase Sign In Success")
+//            
+//            guard let authResult = authResult else {
+//                let error = NSError(domain: "NS Error for AuthResult", code: -1)
+//                completion(.none, error)
+//                return
+//            }
+//            
+//            authResult.user.getIDToken(completion: { (token, error) in
+//                if let error = error {
+//                    completion(.none, error)
+//                    return
+//                }
+//                
+//                guard let accessToken = token else {
+//                    let error = NSError(domain: "accessToken Error", code: -1)
+//                    completion(.none, error)
+//                    return
+//                }
+//                
+//                guard let fbUser = Auth.auth().currentUser else {
+//                    let error = NSError(domain: "fbUser Error", code: -1)
+//                    completion(.none, error)
+//                    return
+//                    
+//                }
+//                
+//                self.fbUserId = fbUser.uid
+//                
+//                print("accessToken: \(accessToken)")
+//                print("fbUserId: \(self.fbUserId)")
+                
+                //위에서 받은 fbUserId와 appleCredential을 addUserData func을 call 해 database에 저장
+//                self.addUserData(appleCredential: appleCredential) { needToGetAuthStatus, error in
+//                    if let error = error {
+//                        completion(.none, error)
+//                        return
+//                    }
+//                    
+//                    //Setting userDefault isLogin to keep currentUser data until app is deleted
+//                    UserDefaults.standard.set(self.fbUserId, forKey: "isLogin") //userDefaults에 fbUserId (fbID)를 저장
+//                    completion(needToGetAuthStatus, nil) //성공할경우
+//                    
+//                }
+//            })
+//        }
+//                5/16 일단 commented===
+        
+    }
+    
+    
+    
 }
--- GroupBuilder/GroupBuilder/VCs/Login/LoginVC.swift
+++ GroupBuilder/GroupBuilder/VCs/Login/LoginVC.swift
@@ -15,6 +15,8 @@ class LoginVC: UIViewController, UITabBarControllerDelegate {
     lazy var contentView: LoginView = LoginView()
     let model: LoginModel = LoginModel()
 
+    let alert = UIAlertController()
+    
     //For Tabbar (1/2)
     let tabBarCtlr = UITabBarController()
     
@@ -31,6 +33,18 @@ class LoginVC: UIViewController, UITabBarControllerDelegate {
             
     }
     
+    // === Step 1. Apple 로그인 버튼 탭. Continues in Model === //
+    @objc func appleLoginButtonPressed() {
+        
+        let authorizationController = model.startSignInWithAppleFlow()
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+//        self.present(self.tabBarCtlr, animated: true) // LoginVC -> TabBar
+ 
+    }
+    
+    //VCs for TabBarCtlr
     func navigationVCs() {
         
         let todayVC: TodayVC = TodayVC()
@@ -59,12 +73,97 @@ class LoginVC: UIViewController, UITabBarControllerDelegate {
         tabBarCtlr.setViewControllers([todayNavi, calendarNavi, progressNavi], animated: false)
         
     }
-    
-    @objc func appleLoginButtonPressed() {
-        
-        self.present(self.tabBarCtlr, animated: true) // LoginVC -> TabBar
- 
-    }
 
 }
 
+extension LoginVC: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    
+    /* === Step 3. 애플 인증 처리 - AppleID, idToken. ========
+     
+     idToken: 안에는 나의 아이디랑 패스워드가 들어가 있는데 이것을 그대로 보냈을때 보안에 문제가 생길수 있으니, 이것을 한 번 더 감써서 안전하게 보내는것. 이것을 OAuth라고 부른다.
+     
+     */
+    
+    //Must 생성 (auto) when you create authorizationController in step 1
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            
+            guard let appleIdToken = appleIDCredential.identityToken,
+                  let idTokenString = String(data: appleIdToken, encoding: .utf8) else {
+                
+                let error = NSError(domain: "Unable to fetch identity token and serialize token string from data", code: -1)
+                
+                alert.title = "Problem with Apple Login"
+                alert.message = "\(error)"
+                
+                return
+                
+            }
+            
+            
+            
+            model.authApple(idTokenString: idTokenString, appleIDCredential: appleIDCredential) { needToGetAuthStatus, error in
+                
+//                5/16 일단 commented     ===
+//                if let error = error {
+//                    print(error.localizedDescription)
+//                    return
+//                }
+//                5/16 일단 commented     ===
+
+//              DELETE after adding FB ===
+                
+                switch needToGetAuthStatus {
+                
+                case .presentTabBar:
+                    self.present(self.tabBarCtlr, animated: true) // LoginVC -> TabBar
+                    
+                case .none:
+                    return
+                    
+                case .needToGetUserName:
+                    return
+                }
+//                DELETE after adding FB ===
+               
+//                5/16 일단 commented===
+
+//                switch needToGetAuthStatus {
+//                    
+//                case .presentTabBar:
+//                    guard let fbUserIdReceived = self.model.fbUserId else {return}
+//
+//                    self.ledgerTableVC.model.fbId = fbUserIdReceived
+//                    self.ledgerTableVC.fbId = fbUserIdReceived
+//                    
+//                    self.present(self.tabBarCtlr, animated: true)
+//                    
+//                case .needToGetUserName:
+//                    print("LoginVC - needToGetUserName")
+//                    guard let fbUserIdReceived = self.model.fbUserId else {return}
+//                    
+//                    let userNameVC = UserNameVC(fbId: fbUserIdReceived)
+//                    userNameVC.delegate = self
+//                    
+//                    self.present(userNameVC, animated: true)
+//                    
+//                case .none:
+//                    return
+//                }
+//                5/16 일단 commented===
+
+            }
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print("Sign in with Apple errored: \(error)")
+    }
+    
+}
